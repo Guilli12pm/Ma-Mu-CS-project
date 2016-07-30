cfdf1560fe00057ee0b8e44730d83d4b558a7b42
adrien <aarculeo@1024degres.com>
2016-07-30 14:51:06 +0100

    Midi to CSV Initial commit of raw CSV files

---
+++ 01_Midi_to_CSV/csv_reposition.php
@@ -0,0 +1,31 @@
+<?php
+$row = 1;
+if (($handle = fopen($argv[1], "r")) !== FALSE)
+{
+	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+	{
+		$track = $data[0];
+		$code = $data[2];
+		switch($code)
+		{
+			case 'Tempo':
+				$tempo = $data[3];
+				break;
+			case 'note_on_c':
+				$note = $data[4];
+				$duration = $data[5] * $tempo;
+			default:
+		}
+		$timestamp = $data[1]
+		$instrument = $data[2];
+		$frequency = $data[4];
+		$duration = $data[5];
+		$ouput[$track]["instrument"] = 
+		}
+		$row++;
+
+	}
+	fclose($handle);
+}
+
+?>
